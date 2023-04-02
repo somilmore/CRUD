b91818e6b61ddc89d7de81613365a34da78738d3
Somil More <117371840+somilmore@users.noreply.github.com>
2023-04-02 19:13:37 +0530

    Add files via upload

---
+++ CRUD_Firebase-main/CRUD_Firebase-main/fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC4y7BVhJoopkf6rJh9JF97YlkWZ15ecJs",
+  authDomain: "cloud-73da9.firebaseapp.com",
+  databaseURL: "https://cloud-73da9-default-rtdb.firebaseio.com",
+  projectId: "cloud-73da9",
+  storageBucket: "cloud-73da9.appspot.com",
+  messagingSenderId: "167190641030",
+  appId: "1:167190641030:web:5a42747329cfaaeef75407",
+  measurementId: "G-CMZZYEWN72"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
